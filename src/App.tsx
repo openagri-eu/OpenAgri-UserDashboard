341fd209c6717a2253a76ff57e40f914bf9e5d98
Mateo Budanovic <mateo.budanovic@vizlore.com>
2025-07-04 15:31:12 +0200

    installed vite-tsconfig-paths and configured path aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import HomeIcon from '@mui/icons-material/Home';
 import { ReactRouterAppProvider } from '@toolpad/core/react-router';
 import { Outlet } from 'react-router';
 import type { Navigation } from '@toolpad/core';
-import { theme } from './theme/theme.tsx';
+import { theme } from '@theme/theme';
+
+import HomeIcon from '@mui/icons-material/Home';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import ThermostatIcon from '@mui/icons-material/Thermostat';
+import WaterDropIcon from '@mui/icons-material/WaterDrop';
+import PestControlIcon from '@mui/icons-material/PestControl';
+import AssessmentIcon from '@mui/icons-material/Assessment'; // TODO: possibly change to something else
 
 const NAVIGATION: Navigation = [
   {
@@ -13,6 +19,35 @@ const NAVIGATION: Navigation = [
     segment: '',
     title: 'Home page',
     icon: <HomeIcon />,
+  },
+  {
+    kind: 'header',
+    title: 'Services',
+  },
+  {
+    segment: 'farm-calendar',
+    title: 'Farm calendar',
+    icon: <CalendarMonthIcon />,
+  },
+  {
+    segment: 'weather-data',
+    title: 'Weather data',
+    icon: <ThermostatIcon />,
+  },
+  {
+    segment: 'pdm',
+    title: 'Pest and disease management',
+    icon: <PestControlIcon />,
+  },
+  {
+    segment: 'irrigation',
+    title: 'Irrigation management',
+    icon: <WaterDropIcon />,
+  },
+  {
+    segment: 'reporting-service',
+    title: 'Reporting service',
+    icon: <AssessmentIcon />,
   },
 ];
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
+  plugins: [react(), tsconfigPaths(), VitePWA({
     registerType: 'autoUpdate',
     injectRegister: 'auto',
 
