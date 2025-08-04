323a0fb15d1b32fec0ffb35e0dad61a758b9fec9
Mateo Budanovic <mateo.budanovic@vizlore.com>
2025-08-04 14:23:15 +0200

    added api url to vite env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/react" />
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
