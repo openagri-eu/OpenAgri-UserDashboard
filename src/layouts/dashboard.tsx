f9ce49d6a01361b43a0c20e5fa444a4e1f5e4028
Mateo Budanovic <mateo.budanovic@vizlore.com>
2025-08-08 17:13:47 +0200

    parcel selection module margin added to dashboard

--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -2,12 +2,15 @@ import { Outlet } from 'react-router';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { PageContainer } from '@toolpad/core/PageContainer';
 import ParcelSelectionModule from '@components/shared/ParcelSelectionModule/ParcelSelectionModule';
+import { Box } from '@mui/material';
 
 export default function DashLayout() {
   return (
     <DashboardLayout>
       <PageContainer>
-        <ParcelSelectionModule></ParcelSelectionModule>
+        <Box sx={{ marginBottom: 2 }}>
+          <ParcelSelectionModule></ParcelSelectionModule>
+        </Box>
         <Outlet />
       </PageContainer>
     </DashboardLayout>
