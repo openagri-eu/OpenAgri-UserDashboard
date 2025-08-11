00a25e078ab01a04e7781582da588a91ed117bf9
Mateo Budanovic <mateo.budanovic@vizlore.com>
2025-08-11 15:15:37 +0200

    implemented toolbar actions

---
+++ src/components/ToolbarActions/ToolbarActions.tsx
@@ -0,0 +1,70 @@
+import { IconButton, ListItemIcon, ListItemText, MenuItem, MenuList, Paper, Popover, Tooltip } from "@mui/material";
+import { useCallback, useState } from "react";
+
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useNavigate } from "react-router-dom";
+import { useSession } from "@contexts/SessionContext";
+
+const ToolbarActions = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [menuAnchorEl, setMenuAnchorEl] = useState<HTMLElement | null>(null);
+
+    const toggleMenu = useCallback(
+        (event: React.MouseEvent<HTMLElement>) => {
+            setMenuAnchorEl(isMenuOpen ? null : event.currentTarget);
+            setIsMenuOpen((previousIsMenuOpen) => !previousIsMenuOpen);
+        },
+        [isMenuOpen],
+    );
+
+    const { setSession } = useSession();
+    const navigate = useNavigate();
+
+    const handleSignOut = () => {
+        setSession(null);
+        navigate("/");
+    }
+
+    return (
+        <>
+            <Tooltip title="Settings" enterDelay={1000}>
+                <div>
+                    <IconButton type="button" aria-label="settings" onClick={toggleMenu}>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </Tooltip>
+            <Popover
+                open={isMenuOpen}
+                anchorEl={menuAnchorEl}
+                onClose={toggleMenu}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                disableAutoFocus
+            >
+                <Paper sx={{ width: 200, maxWidth: '100%' }}>
+                    <MenuList>
+                        {/* <MenuItem>
+                            <ListItemIcon>
+                                <AccountCircleIcon fontSize="small"/>
+                            </ListItemIcon>
+                            <ListItemText>Account</ListItemText>
+                        </MenuItem> */}
+                        {/* <Divider /> */}
+                        <MenuItem onClick={handleSignOut}>
+                            <ListItemIcon>
+                                <LogoutIcon fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText>Sign Out</ListItemText>
+                        </MenuItem>
+                    </MenuList>
+                </Paper>
+            </Popover>
+        </>
+    );
+}
+
+export default ToolbarActions;
--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -3,10 +3,15 @@ import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { PageContainer } from '@toolpad/core/PageContainer';
 import ParcelSelectionModule from '@components/shared/ParcelSelectionModule/ParcelSelectionModule';
 import { Box } from '@mui/material';
+import ToolbarActions from '@components/ToolbarActions/ToolbarActions';
 
 export default function DashLayout() {
   return (
-    <DashboardLayout>
+    <DashboardLayout
+      slots={{
+        toolbarActions: ToolbarActions
+      }}
+    >
       <PageContainer>
         <Box sx={{ marginBottom: 2 }}>
           <ParcelSelectionModule></ParcelSelectionModule>
