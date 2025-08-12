4e6f1ee2a2875b4912a163b5ca0ae554017cb38d
Mateo Budanovic <mateo.budanovic@vizlore.com>
2025-08-12 16:48:46 +0200

    implemented generic sortable table and integrated it into the farm calendar page

--- src/components/dashboard/services/FarmCalendar.tsx
+++ src/components/dashboard/services/FarmCalendar.tsx
@@ -1,4 +1,5 @@
 import GenericSnackbar from "@components/shared/GenericSnackbar/GenericSnackbar";
+import GenericSortableTable, { HeadCell } from "@components/shared/GenericSortableTable/GenericSortableTable";
 import useFetch from "@hooks/useFetch";
 import useSnackbar from "@hooks/useSnackbar";
 import { FarmParcel } from "@models/FarmParcel";
@@ -8,6 +9,7 @@ import { useEffect, useState } from "react";
 const FarmCalendar = () => {
 
     const [parcels, setParcels] = useState<FarmParcel[]>([]);
+    const [parcelData, setParcelData] = useState<ParcelRow[]>([]);
 
     const { fetchData, loading, response, error } = useFetch<FarmParcel[]>(
         "proxy/farmcalendar/api/v1/FarmParcels/?format=json",
@@ -30,18 +32,51 @@ const FarmCalendar = () => {
 
     useEffect(() => {
         if (response) {
-            setParcels(response)
+            setParcels(response);
+            const formattedParcels = parcels.map((p) => {
+                return {
+                    id: p["@id"],
+                    farm: p.farm["@type"],
+                    identifier: p.identifier,
+                    parcelType: p["@type"],
+                    validFromTo: `From: ${p.validFrom}\nUntil: ${p.validTo}`,
+                    coordinates: `${p.location.lat}\n${p.location.long}`,
+                    timestamps: `Created: ${p.created_at}\nUpdated: ${p.updated_at}`,
+                }
+            })
+            setParcelData(formattedParcels);
         }
     }, [response])
+
+    interface ParcelRow {
+        id: string;
+        farm: string;
+        identifier: string;
+        parcelType: string;
+        validFromTo: string;
+        coordinates: string;
+        timestamps: string;
+    }
+
+    const parcelHeadCells: readonly HeadCell<ParcelRow>[] = [
+        { id: 'farm', numeric: false, label: 'Farm' },
+        { id: 'identifier', numeric: false, label: 'Identifier' },
+        { id: 'parcelType', numeric: false, label: 'Parcel Type' },
+        { id: 'validFromTo', numeric: false, label: 'Valid from - Valid until' },
+        { id: 'coordinates', numeric: false, label: 'Coordinates' },
+        { id: 'timestamps', numeric: false, label: 'Timestamps' },
+    ];
+
+    const handleRowClick = (parcel: ParcelRow) => {
+        console.log("Clicked on parcel:", parcel.identifier);
+    };
 
     return (
         <>
             {loading && <Skeleton variant="rectangular" height={48} />}
             {
                 !loading && !error &&
-                parcels.map((p) => {
-                    return <div>{p.identifier}</div>
-                })
+                <GenericSortableTable data={parcelData} headCells={parcelHeadCells} onRowClick={handleRowClick}></GenericSortableTable>
             }
             <GenericSnackbar
                 type={snackbarState.type}
---
+++ src/components/shared/GenericSortableTable/GenericSortableTable.tsx
@@ -0,0 +1,205 @@
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Paper from '@mui/material/Paper';
+import { visuallyHidden } from '@mui/utils';
+import { useMemo, useState } from 'react';
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = 'asc' | 'desc';
+
+function getComparator<T>(
+  order: Order,
+  orderBy: keyof T,
+): (a: T, b: T) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+export interface HeadCell<T> {
+  id: keyof T;
+  label: string;
+  numeric: boolean;
+}
+
+interface EnhancedTableHeadProps<T> {
+  onRequestSort: (event: React.MouseEvent<unknown>, property: keyof T) => void;
+  order: Order;
+  orderBy: keyof T | null;
+  headCells: readonly HeadCell<T>[];
+}
+
+function EnhancedTableHead<T>(props: EnhancedTableHeadProps<T>) {
+  const { order, orderBy, onRequestSort, headCells } = props;
+  const createSortHandler =
+    (property: keyof T) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id as string}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+interface GenericSortableTableProps<T> {
+  data: readonly T[];
+  headCells: readonly HeadCell<T>[];
+  onRowClick?: (row: T) => void;
+}
+
+function GenericSortableTable<T extends { id: number | string }>({
+  data,
+  headCells,
+  onRowClick,
+}: GenericSortableTableProps<T>) {
+  const [order, setOrder] = useState<Order>('asc');
+  const [orderBy, setOrderBy] = useState<keyof T | null>(null);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleRequestSort = (
+    _: React.MouseEvent<unknown>,
+    property: keyof T,
+  ) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (_: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - data.length) : 0;
+
+  const visibleRows = useMemo(() => {
+    const sortableData = orderBy
+      ? [...data].sort(getComparator(order, orderBy))
+      : [...data];
+
+    return sortableData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+  }, [data, order, orderBy, page, rowsPerPage]);
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <TableContainer sx={{ maxHeight: 500 }}>
+          <Table
+            stickyHeader
+            sx={{ minWidth: 750 }}
+            size={'medium'}
+          >
+            <EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+              headCells={headCells}
+            />
+            <TableBody>
+              {visibleRows.map((row, index) => {
+                const labelId = `generic-table-row-${index}`;
+                return (
+                  <TableRow
+                    hover
+                    tabIndex={-1}
+                    key={row.id}
+                    sx={{ cursor: 'pointer' }}
+                    onClick={() => onRowClick && onRowClick(row)}
+                  >
+                    {headCells.map((cell, cellIndex) => {
+                      const cellValue = row[cell.id] as React.ReactNode;
+                      if (cellIndex === 0) {
+                        return (
+                          <TableCell
+                            key={cell.id as string}
+                            component="th"
+                            id={labelId}
+                            scope="row"
+                            padding={'normal'}
+                          >
+                            {cellValue}
+                          </TableCell>
+                        );
+                      }
+                      return (
+                        <TableCell
+                          key={cell.id as string}
+                          sx={{ whiteSpace: 'pre-line' }}
+                          align={cell.numeric ? 'right' : 'left'}
+                        >
+                          {cellValue}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: (53) * emptyRows }}>
+                  <TableCell colSpan={headCells.length} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
+  );
+}
+
+export default GenericSortableTable;
