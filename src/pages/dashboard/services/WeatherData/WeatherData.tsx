6803e30d0eff28ffeacee83de5ba7044eaf59dd7
Mateo Budanovic <mateo.budanovic@vizlore.com>
2025-09-11 12:31:18 +0200

    weather data set data to null on error from response

--- src/pages/dashboard/services/WeatherData/WeatherData.tsx
+++ src/pages/dashboard/services/WeatherData/WeatherData.tsx
@@ -83,6 +83,7 @@ const WeatherDataPage = () => {
     useEffect(() => {
         if (error) {
             showSnackbar('error', 'Error loading weather');
+            setFormattedData(null);
         }
     }, [error])
 
