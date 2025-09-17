d7e1987744880e65ea434ddfb8ca6fbb67e0c45e
Mateo Budanovic <mateo.budanovic@vizlore.com>
2025-09-17 17:13:20 +0200

    diseases displayed in table and details in dialog

--- src/pages/dashboard/services/PestAndDisease/Diseases.tsx
+++ src/pages/dashboard/services/PestAndDisease/Diseases.tsx
@@ -1,27 +1,32 @@
+import GenericDialog from "@components/shared/GenericDialog/GenericDialog";
 import GenericSortableTable from "@components/shared/GenericSortableTable/GenericSortableTable";
 import { HeadCell } from "@components/shared/GenericSortableTable/GenericSortableTable.types";
+import useDialog from "@hooks/useDialog";
 import useFetch from "@hooks/useFetch";
 import { DiseasesResponseModel } from "@models/Disease";
-import { Box, Skeleton } from "@mui/material";
+import { Box, Card, CardContent, Skeleton, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
 const DiseasesPage = () => {
     const [diseases, setDiseases] = useState<DiseaseRow[]>([]);
+    const [selectedDisease, setSelectedDisease] = useState<DiseaseRow | null>(null);
 
-    const { fetchData, response, error, loading } = useFetch<DiseasesResponseModel>(
+    const { fetchData: getFetchData, response: getResponse, error: getError, loading: getLoading } = useFetch<DiseasesResponseModel>(
         `proxy/pdm/api/v1/disease/`,
         {
             method: 'GET',
         }
     );
 
+    const { dialogProps, showDialog } = useDialog();
+
     useEffect(() => {
-        fetchData();
+        getFetchData();
     }, []);
 
     useEffect(() => {
-        if (response) {
-            const formattedDiseases = response.diseases.map((d) => {
+        if (getResponse) {
+            const formattedDiseases = getResponse.diseases.map((d) => {
                 return {
                     id: d.id,
                     name: d.name,
@@ -33,7 +38,7 @@ const DiseasesPage = () => {
             })
             setDiseases(formattedDiseases);
         }
-    }, [response])
+    }, [getResponse])
 
     interface DiseaseRow {
         id: string;
@@ -51,24 +56,58 @@ const DiseasesPage = () => {
 
     const diseasesHeadCells: readonly HeadCell<DiseaseRow>[] = [
         { id: 'name', numeric: false, label: 'Name' },
-        { id: 'description', numeric: false, label: 'Description' },
         { id: 'eppoCode', numeric: false, label: 'EPPO code' },
         { id: 'baseGDD', numeric: false, label: 'Base GDD' },
     ];
 
     const handleRowClick = (disease: DiseaseRow) => {
-        console.log(disease);
+        setSelectedDisease(disease);
+        showDialog({
+            title: `Details for ${disease.name}`,
+            variant: 'empty',
+            children: <></>
+        });
+    };
+
+    const handleCloseDialog = () => {
+        dialogProps.onClose();
+        setSelectedDisease(null);
     };
 
     return (
         <>
             <Box display={'flex'} flexDirection={'column'} gap={2}>
-                {loading && <Skeleton variant="rectangular" height={48} />}
+                {getLoading && <Skeleton variant="rectangular" height={48} />}
                 {
-                    !loading && !error &&
+                    !getLoading && !getError &&
                     <GenericSortableTable data={diseases} headCells={diseasesHeadCells} onRowClick={handleRowClick}></GenericSortableTable>
                 }
             </Box>
+            <GenericDialog {...dialogProps} onClose={handleCloseDialog}>
+                <>
+                    <Box display={'flex'} flexDirection={'column'} gap={2}>
+                        <Typography variant="body1">
+                            Disease description: {selectedDisease?.description}
+                        </Typography>
+                        <Typography variant="body1">
+                            EPPO code: {selectedDisease?.eppoCode}
+                        </Typography>
+                        <Typography variant="body1">
+                            Base GDD: {selectedDisease?.baseGDD}
+                        </Typography>
+                        <Typography variant="h6">GDD points</Typography>
+                        {selectedDisease?.gddPoints.map(gddp => {
+                            return <Card key={"GDDP-ID-" + gddp.id}>
+                                <CardContent>
+                                    <div>From: {gddp.start}</div>
+                                    <div>To: {gddp.end}</div>
+                                    <div>Descriptor: {gddp.descriptor}</div>
+                                </CardContent>
+                            </Card>
+                        })}
+                    </Box>
+                </>
+            </GenericDialog>
         </>
     )
 }
