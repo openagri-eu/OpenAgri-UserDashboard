9dbe4950f2e9e09256d30e35d197acdcac15932a
Mateo Budanovic <mateo.budanovic@vizlore.com>
2025-09-24 11:03:48 +0200

    theme now using eggshell background

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -6,10 +6,10 @@ import './typography'
 const themeOptions: ThemeOptions = {
     palette: {
         mode: 'light',
-        // background: {
-        //     default: '#fffdf5',
-        //     paper: '#fffbeb'
-        // },
+        background: {
+            default: '#fffdf5',
+            paper: '#fffbeb'
+        },
         primary: {
             main: colors.primary.main,
         },
