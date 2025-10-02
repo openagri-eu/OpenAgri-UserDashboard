911d0886578cf742e45e079e6a82a32d42355fb7
Mateo Budanovic <mateo.budanovic@vizlore.com>
2025-10-02 13:07:12 +0200

    spray forecast color chips changes and overall text clarification

--- src/components/dashboard/services/SprayForecast/SprayForecast.tsx
+++ src/components/dashboard/services/SprayForecast/SprayForecast.tsx
@@ -4,7 +4,7 @@ import { useSession } from "@contexts/SessionContext";
 import useFetch from "@hooks/useFetch";
 import useSnackbar from "@hooks/useSnackbar";
 import { SprayForecastModel, SprayStatus } from "@models/SprayForecast";
-import { Box, Card, CardContent, Chip, Skeleton, SvgIcon, Typography } from "@mui/material";
+import { Box, Card, CardContent, Chip, Divider, Skeleton, SvgIcon, Typography } from "@mui/material";
 import dayjs from "dayjs";
 import { Fragment, useEffect, useState } from "react";
 
@@ -34,6 +34,13 @@ const getStatusColor = (status?: SprayStatus): ChipProps['color'] => {
             return 'success';
         case 'marginal':
             return 'warning';
+        case 'unsuitable':
+            return 'error';
+    }
+};
+
+const getDetailsColor = (status?: SprayStatus): ChipProps['color'] => {
+    switch (status) {
         case 'unsuitable':
             return 'error';
         default:
@@ -76,7 +83,7 @@ const SprayForecast = () => {
 
                 accumulator[date][time].spray_conditions = spray_conditions;
                 accumulator[date][time].detailed_status = detailed_status;
-                
+
                 setLoadingFormat(false);
                 return accumulator;
             }, {});
@@ -95,9 +102,9 @@ const SprayForecast = () => {
                             return <Card key={`id-date-${date}`}>
                                 <CardContent>
                                     <Typography gutterBottom variant="h4">{dayjs(date).format('dddd, D/MMM/YYYY')}</Typography>
-                                    {/* Added sx prop for better responsive behavior */}
                                     <Box display={'flex'} flexDirection={'column'} gap={2} sx={{ overflowX: 'auto', py: 1 }}>
                                         {Object.entries(dailyData).sort().map(([time, timeData]: [string, TimeData]) => {
+                                            const sprayConditions = timeData.spray_conditions ? timeData.spray_conditions?.charAt(0).toUpperCase() + timeData.spray_conditions?.slice(1) : '';
                                             return <Fragment key={`id-time-${date}-${time}`}>
                                                 <Box display={'flex'} flex={1} justifyContent={'space-between'} alignItems={'center'} gap={2} sx={{ minWidth: 800 }}>
                                                     <Box display={'flex'} alignItems={'center'} gap={1} flex={1}>
@@ -105,22 +112,20 @@ const SprayForecast = () => {
                                                         {dayjs(`${date} ${time}`).format('HH:mm')}
                                                     </Box>
                                                     <Box display={'flex'} flex={1} justifyContent={'center'}>
-                                                        <Chip label="Overall" color={getStatusColor(timeData.spray_conditions)} />
+                                                        <Chip label={sprayConditions} color={getStatusColor(timeData.spray_conditions)} />
+                                                    </Box>
+                                                    <Divider orientation="vertical" flexItem />
+                                                    <Box display={'flex'} flex={1} justifyContent={'center'}>
+                                                        <Chip label="Humidity" color={getDetailsColor(timeData.detailed_status?.humidity_status)} />
                                                     </Box>
                                                     <Box display={'flex'} flex={1} justifyContent={'center'}>
-                                                        <Chip label="Delta T" color={getStatusColor(timeData.detailed_status?.delta_t_status)} />
+                                                        <Chip label="Precipitation" color={getDetailsColor(timeData.detailed_status?.precipitation_status)} />
                                                     </Box>
                                                     <Box display={'flex'} flex={1} justifyContent={'center'}>
-                                                        <Chip label="Humidity" color={getStatusColor(timeData.detailed_status?.humidity_status)} />
+                                                        <Chip label="Temperature" color={getDetailsColor(timeData.detailed_status?.temperature_status)} />
                                                     </Box>
                                                     <Box display={'flex'} flex={1} justifyContent={'center'}>
-                                                        <Chip label="Precipitation" color={getStatusColor(timeData.detailed_status?.precipitation_status)} />
-                                                    </Box>
-                                                    <Box display={'flex'} flex={1} justifyContent={'center'}>
-                                                        <Chip label="Temperature" color={getStatusColor(timeData.detailed_status?.temperature_status)} />
-                                                    </Box>
-                                                    <Box display={'flex'} flex={1} justifyContent={'center'}>
-                                                        <Chip label="Wind" color={getStatusColor(timeData.detailed_status?.wind_status)} />
+                                                        <Chip label="Wind" color={getDetailsColor(timeData.detailed_status?.wind_status)} />
                                                     </Box>
                                                 </Box>
                                             </Fragment>
