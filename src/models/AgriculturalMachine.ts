ea56ec0b57822f0867b999d8695b32137cf15e4d
Mateo Budanovic <mateo.budanovic@vizlore.com>
2025-10-24 13:11:27 +0200

    impl agricultural machine interface and used it in UAV flight forecast component

---
+++ src/models/AgriculturalMachine.ts
@@ -0,0 +1,14 @@
+export interface AgriculturalMachine {
+    id: string;
+    name: string;
+    description: string;
+    hasAgriParcel: string;
+    purchase_date: string;
+    manufacturer: string;
+    model: string;
+    seria_number: string;
+    status: number;
+    invalidatedAtTime: string;
+    dateCreated: string;
+    dateModified: string;
+}
