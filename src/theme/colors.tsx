7901b3ff8c50f9abe81d737387ca4c979a3ec38e
Mateo Budanovic <mateo.budanovic@vizlore.com>
2026-06-16 10:10:59 +0200

    extracted colors from theme as env vars to be used by docker files

--- src/theme/colors.tsx
+++ src/theme/colors.tsx
@@ -1,12 +1,23 @@
 import { lighten } from "@mui/material";
 
+const env = import.meta.env;
+
+const PRIMARY = env.VITE_PRIMARY_COLOR || '#558bc9';
+const SECONDARY = env.VITE_SECONDARY_COLOR || '#c8cc27';
+const BG_DEFAULT = env.VITE_BACKGROUND_DEFAULT || '#fffdf5';
+const BG_PAPER = env.VITE_BACKGROUND_PAPER || '#fffbeb';
+
 export const colors = {
     primary: {
-        main: '#558bc9',
-        background: lighten('#558bc9', 0.9)
+        main: PRIMARY,
+        background: lighten(PRIMARY, 0.9)
     },
     secondary: {
-        main: '#c8cc27',
+        main: SECONDARY,
+    },
+    background: {
+        default: BG_DEFAULT,
+        paper: BG_PAPER,
     },
     other: {
         lightgrey: "#fafbfb",
