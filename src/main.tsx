42b33497b627236382e07dc7fbd9f290deee8c3d
Mateo Budanovic <mateo.budanovic@vizlore.com>
2026-09-08 13:22:01 +0200

    reenabled register

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ import GrowingDegreeDaysPage from '@pages/dashboard/services/PestAndDisease/Grow
 import CompostOperationsReportPage from '@pages/dashboard/services/FarmCalendar/ReportingService/CompostOperations.tsx';
 import AuthLayout from '@layouts/auth.tsx';
 import SignInPage from '@pages/auth/SignIn/SignInPage.tsx';
+import SignUpPage from '@pages/auth/SignUp/SignUpPage.tsx';
 import FarmParcelPage from '@pages/dashboard/services/FarmCalendar/FarmLocations/FarmParcel.tsx';
 import FarmParcelsPage from '@pages/dashboard/services/FarmCalendar/FarmLocations/FarmParcels.tsx';
 import FarmsPage from '@pages/dashboard/services/FarmCalendar/FarmLocations/Farms.tsx';
@@ -257,6 +258,10 @@ const router = createBrowserRouter([
           {
             path: 'sign-in',
             Component: SignInPage
+          },
+          {
+            path: 'sign-up',
+            Component: SignUpPage
           },
           {
             path: 'session-refresh',
